e7bed23d0acfdad8cc7587e75b2d6ae1d7fb9dcc
huitopia <huitopia27@gmail.com>
2024-05-07 14:06:18 +0900

    table, field 생성

--- sql/prj2.sql
+++ sql/prj2.sql
@@ -1,3 +1,45 @@
 CREATE DATABASE prj2;
 
 USE prj2;
+
+CREATE TABLE member
+(
+    id            INT PRIMARY KEY AUTO_INCREMENT,
+    email         VARCHAR(20) NOT NULL UNIQUE,
+    password      VARCHAR(20) NOT NULL,
+    nick_name     VARCHAR(10) NOT NULL UNIQUE,
+    inserted_date DATETIME    NOT NULL DEFAULT NOW()
+);
+
+DESC member;
+
+CREATE TABLE authority
+(
+    id        INT PRIMARY KEY AUTO_INCREMENT,
+    name      VARCHAR(20) NOT NULL DEFAULT ('member'),
+    member_id INT         NOT NULL REFERENCES member (id)
+);
+
+desc authority;
+
+CREATE TABLE board
+(
+    id            INT PRIMARY KEY AUTO_INCREMENT,
+    title         VARCHAR(20)   NOT NULL,
+    content       VARCHAR(1000) NOT NULL,
+    inserted_date DATETIME      NOT NULL DEFAULT NOW(),
+    member_id     INT           NOT NULL REFERENCES member (id)
+);
+
+DESC board;
+
+CREATE TABLE comment
+(
+    id            INT PRIMARY KEY AUTO_INCREMENT,
+    comment       VARCHAR(50) NOT NULL,
+    inserted_date DATETIME    NOT NULL DEFAULT NOW(),
+    member_id     INT         NOT NULL REFERENCES member (id),
+    board_id      INT         NOT NULL REFERENCES board (id)
+);
+
+DESC comment;
